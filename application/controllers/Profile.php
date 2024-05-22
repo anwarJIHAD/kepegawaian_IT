da5399cb76df36bb203964d4422afae2c04bd9a7
anwar2731 <anwar20ti@mahasiswa.pcr.ac.id>
2024-05-22 16:57:32 +0700

    edit 50%

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -1,18 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Profile extends CI_Controller {
-        public function __construct()
-        {
-            parent::__construct();
-            is_logged_in();
-            
-        }
-
-public function index()
+class Profile extends CI_Controller
 {
-        $this->load->view('layout/header');
-        $this->load->view('Auth/vw_profile');
-        $this->load->view('layout/footer');
-} 
+	public function __construct()
+	{
+		parent::__construct();
+		is_logged_in();
+
+	}
+
+	public function index()
+	{
+		$data['pegawai'] = $this->db->get_where('pegawai', ['username' => $this->session->userdata['username']])->row_array();
+		$this->load->view('layout/header', $data);
+		$this->load->view('Auth/vw_profile', $data);
+		$this->load->view('layout/footer', $data);
+	}
 }
--- application/views/Auth/vw_profile.php
+++ application/views/Auth/vw_profile.php
@@ -5,73 +5,86 @@
 <!-- Main Content -->
 <?= $this->session->flashdata('message'); ?>
 <div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Halaman Edit Profile</h1>
-        </div>
+	<section class="section">
+		<div class="section-header">
+			<h1>Halaman Edit Profile</h1>
+		</div>
 
-        <div class="section-body">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card border rounded shadow-lg p-3 mb-5" >
-                            <div class="card-header">
-                                <h4>Edit Profile</h4>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-5">
-                                    <img src="https://awsimages.detik.net.id/community/media/visual/2023/04/14/gambar-pemandangan-6.jpeg?w=3000" alt="Gambar" class="img-fluid mb-2">
-                                    
-                                </div>
-                                <div class="col-md-7">
-                                    <div class="card-body">
+		<div class="section-body">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-md-8">
+						<div class="card border rounded shadow-lg p-3 mb-5">
+							<div class="card-header">
+								<h4>Edit Profile</h4>
+							</div>
+							<div class="row">
+								<div class="col-md-5">
+									<img src="https://awsimages.detik.net.id/community/media/visual/2023/04/14/gambar-pemandangan-6.jpeg?w=3000"
+										alt="Gambar" class="img-fluid mb-2">
 
-                                        <form method="POST">
-                                            <div class="form-group row">
-                                                <label for="inputEmail3" class="col-sm-3 col-form-label">Nama Lengkap</label>
-                                                <div class="col-sm-9">
-                                                    <input type="email" class="form-control" id="inputEmail3" placeholder="Nama Lengkap">
-                                                </div>
-                                            </div>
-                                            <div class="form-group row">
-                                                <label for="inputEmail3" class="col-sm-3 col-form-label">NIY</label>
-                                                <div class="col-sm-9">
-                                                    <input type="email" class="form-control" id="inputEmail3" placeholder="NIY">
-                                                </div>
-                                            </div>
-                                            <div class="form-group row">
-                                                <label for="inputEmail3" class="col-sm-3 col-form-label">Jenis Kelamin</label>
-                                                <div class="col-sm-9">
-                                                    <input type="email" class="form-control" id="inputEmail3" placeholder="Jenis Kelamin">
-                                                </div>
-                                            </div>
-                                            <div class="form-group row">
-                                                <label for="inputEmail3" class="col-sm-3 col-form-label">No Hp</label>
-                                                <div class="col-sm-9">
-                                                    <input type="email" class="form-control" id="inputEmail3" placeholder="No Hp">
-                                                </div>
-                                            </div>
-                                            <div class="form-group row">
-                                                <label for="inputEmail3" class="col-sm-3 col-form-label">Jabatan</label>
-                                                <div class="col-sm-9">
-                                                    <input type="email" class="form-control" id="inputEmail3" placeholder="Jabatan">
-                                                </div>
-                                            </div>
-                                            <a href="<?= base_url('Console/pegawai') ?>" class="btn btn-light ">Ubah Password</a>
-                                            <a href="<?= base_url('Console/pegawai') ?>" class="btn btn-light">Tutup</a>
-                                            <button type="submit" name="tambah" class="btn btn-success float-right">Batal</button>
-                                        </form>
-                                    </div>
-                                </div>
+								</div>
+								<div class="col-md-7">
+									<div class="card-body">
 
-                            </div>
+										<form method="POST">
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Nama
+													Lengkap</label>
+												<div class="col-sm-9">
+													<input type="email"
+														value="<?= $this->session->userdata('username'); ?>"
+														class="form-control" id="inputEmail3"
+														placeholder="Nama Lengkap">
+												</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">NIY</label>
+												<div class="col-sm-9">
+													<input type="email" value="<?= $this->session->userdata('niy'); ?>"
+														class="form-control" id="inputEmail3" placeholder="NIY"
+														readonly>
+												</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Jenis
+													Kelamin</label>
+												<div class="col-sm-9">
+													<input type="email" class="form-control" id="inputEmail3"
+														value="<?= $pegawai['niy'] ?>" placeholder="Jenis Kelamin">
+												</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">No Hp</label>
+												<div class="col-sm-9">
+													<input type="email" class="form-control" id="inputEmail3"
+														placeholder="No Hp">
+												</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Jabatan</label>
+												<div class="col-sm-9">
+													<input type="email" class="form-control" id="inputEmail3"
+														placeholder="Jabatan">
+												</div>
+											</div>
+											<a href="<?= base_url('Console/pegawai') ?>" class="btn btn-light ">Ubah
+												Password</a>
+											<a href="<?= base_url('Console/pegawai') ?>" class="btn btn-light">Tutup</a>
+											<button type="submit" name="tambah"
+												class="btn btn-success float-right">Batal</button>
+										</form>
+									</div>
+								</div>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
+							</div>
 
-    </section>
+						</div>
+					</div>
+				</div>
+			</div>
+
+	</section>
 </div>
 
 </div>
