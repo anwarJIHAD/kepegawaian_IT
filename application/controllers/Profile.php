6310c00cca15ef27ccf990c37ebb5dd24cbd347b
anwar2731 <anwar20ti@mahasiswa.pcr.ac.id>
2024-05-24 21:07:34 +0700

    edit profile

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -7,14 +7,116 @@ class Profile extends CI_Controller
 	{
 		parent::__construct();
 		is_logged_in();
+		$this->load->model('Pegawai_model');
 
 	}
 
 	public function index()
 	{
 		$data['pegawai'] = $this->db->get_where('pegawai', ['username' => $this->session->userdata['username']])->row_array();
-		$this->load->view('layout/header', $data);
-		$this->load->view('Auth/vw_profile', $data);
-		$this->load->view('layout/footer', $data);
+
+		$this->form_validation->set_rules('nama', 'nama', 'required|trim', [
+			'required' => 'Nama Wajib di isi'
+		]);
+		$this->form_validation->set_rules('username', 'username', 'required|trim', [
+			'required' => 'Username Wajib di isi'
+		]);
+		$this->form_validation->set_rules('tmpt_lahir', 'tmpt_lahir', 'required|trim', [
+			'required' => 'Tempat lahir Wajib di isi'
+		]);
+		$this->form_validation->set_rules('tgl_lahir', 'tgl_lahir', 'required|trim', [
+			'required' => 'Tanggal lahir Wajib di isi'
+		]);
+		$this->form_validation->set_rules('pnd_trkhr', 'pnd_trkhr', 'required|trim', [
+			'required' => 'Pendidikan terakhir Wajib di isi'
+		]);
+
+		$this->form_validation->set_rules('jurusan', 'jurusan', 'required|trim', [
+			'required' => 'Jurusan Wajib di isi'
+		]);
+		$this->form_validation->set_rules('jabatan', 'jabatan', 'required|trim', [
+			'required' => 'Jabatan Wajib di isi'
+		]);
+		$this->form_validation->set_rules('no_hp', 'no_hp', 'required|trim', [
+			'required' => 'No handphone Wajib di isi'
+		]);
+		$this->form_validation->set_rules('pass_lama', 'pass_lama', 'trim|callback_check_password_lama');
+		$this->form_validation->set_rules('pass_baru', 'Password Baru', 'trim|callback_check_passwords');
+
+
+		if ($this->form_validation->run() == false) {
+			$this->load->view('layout/header', $data);
+			$this->load->view('Auth/vw_profile', $data);
+			$this->load->view('layout/footer', $data);
+		} else {
+
+			$data = [
+				'nama' => $this->input->post('nama'),
+				'username' => $this->input->post('username'),
+				'tmpt_lahir' => $this->input->post('tmpt_lahir'),
+				'tgl_lahir' => $this->input->post('tgl_lahir'),
+				'pnd_trkhr' => $this->input->post('pnd_trkhr'),
+				'jurusan' => $this->input->post('jurusan'),
+				'jabatan' => $this->input->post('jabatan'),
+				'no_hp' => $this->input->post('no_hp'),
+			];
+
+			if ($this->input->post('password')) {
+				$data['password'] = password_hash($this->input->post('password'), PASSWORD_DEFAULT);
+			}
+			$user = $this->db->get_where('pegawai', ['niy' => $this->session->userdata('niy')])->row_array();
+			$id = $user['id'];
+			$this->Pegawai_model->update(['id' => $id], $data);
+			$this->session->set_flashdata('message', '<div class="alert alert-success" 
+			role="alert">Profil Berhasil Diubah!</div>');
+			redirect('Profile');
+		}
+	}
+	public function check_passwords($new_password)
+	{
+		$confirm_password = $this->input->post('pass_baru2');
+		$old_password = $this->input->post('pass_lama');
+
+		// Check if the new password or its confirmation is filled and compare them
+		if (!empty($new_password) || !empty($confirm_password)) {
+			if ($new_password !== $confirm_password) {
+				$this->form_validation->set_message('check_passwords', 'Password baru dan konfirmasi password harus sama');
+				return FALSE;
+			}
+		}
+
+		// Check if the new password is filled and the old password is not filled
+		if (!empty($new_password) && empty($old_password)) {
+			$this->form_validation->set_message('check_passwords', 'Password lama wajib diisi jika password baru diisi');
+			return FALSE;
+		}
+
+		return TRUE;
+	}
+
+	public function check_password_lama($old_password)
+	{
+		$new_password = $this->input->post('pass_baru');
+		$user = $this->db->get_where('pegawai', ['niy' => $this->session->userdata('niy')])->row_array();
+
+		// Check if the old password is filled and validate it
+		if (!empty($old_password)) {
+			if ($user) {
+				if (password_verify($old_password, $user['password'])) {
+					if (empty($old_password) && !empty($new_password)) {
+						$this->form_validation->set_message('check_password_lama', 'Password lama wajib diisi jika password baru diisi');
+						return FALSE;
+					}
+					if (!empty($old_password) && empty($new_password)) {
+						$this->form_validation->set_message('check_password_lama', 'Password baru wajib diisi jika password lama diisi');
+						return FALSE;
+					}
+					return TRUE;
+				} else {
+					$this->form_validation->set_message('check_password_lama', 'Password lama salah');
+					return FALSE;
+				}
+			}
+		}
 	}
 }
--- application/views/Auth/vw_profile.php
+++ application/views/Auth/vw_profile.php
@@ -3,7 +3,6 @@
 </style>
 
 <!-- Main Content -->
-<?= $this->session->flashdata('message'); ?>
 <div class="main-content">
 	<section class="section">
 		<div class="section-header">
@@ -18,61 +17,144 @@
 							<div class="card-header">
 								<h4>Edit Profile</h4>
 							</div>
-							<div class="row">
-								<div class="col-md-5">
-									<img src="https://awsimages.detik.net.id/community/media/visual/2023/04/14/gambar-pemandangan-6.jpeg?w=3000"
-										alt="Gambar" class="img-fluid mb-2">
+							<?= $this->session->flashdata('message'); ?>
 
-								</div>
-								<div class="col-md-7">
+							<div class="row">
+
+								<div class="col-md-10">
 									<div class="card-body">
 
 										<form method="POST">
 											<div class="form-group row">
-												<label for="inputEmail3" class="col-sm-3 col-form-label">Nama
-													Lengkap</label>
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Nama</label>
 												<div class="col-sm-9">
-													<input type="email"
-														value="<?= $this->session->userdata('username'); ?>"
-														class="form-control" id="inputEmail3"
-														placeholder="Nama Lengkap">
+													<input type="text" value="<?= $pegawai['nama'] ?>"
+														class="form-control" id="inputEmail3" name="nama"
+														placeholder="Username">
+													<?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
+
+												</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Usrname</label>
+												<div class="col-sm-9">
+													<input type="text"
+													value="<?= $pegawai['username'] ?>"
+														class="form-control" id="inputEmail3" name="username"
+														placeholder="Username">
+													<?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
+
 												</div>
 											</div>
 											<div class="form-group row">
 												<label for="inputEmail3" class="col-sm-3 col-form-label">NIY</label>
 												<div class="col-sm-9">
-													<input type="email" value="<?= $this->session->userdata('niy'); ?>"
+													<input type="email" value="<?= $pegawai['niy'] ?>"
 														class="form-control" id="inputEmail3" placeholder="NIY"
-														readonly>
+														name="niy" readonly>
 												</div>
 											</div>
 											<div class="form-group row">
-												<label for="inputEmail3" class="col-sm-3 col-form-label">Jenis
-													Kelamin</label>
+												<label for="inputEmail3" class="col-sm-3 col-form-label">
+													Tempat Lahir</label>
 												<div class="col-sm-9">
-													<input type="email" class="form-control" id="inputEmail3"
-														value="<?= $pegawai['niy'] ?>" placeholder="Jenis Kelamin">
+													<input type="text" class="form-control" id="inputEmail3"
+														name="tmpt_lahir" value="<?= $pegawai['tmpt_lahir'] ?>"
+														placeholder="Jenis Kelamin">
+													<?= form_error('tmpt_lahir', '<small class="text-danger pl-3">', '</small>'); ?>
 												</div>
 											</div>
 											<div class="form-group row">
-												<label for="inputEmail3" class="col-sm-3 col-form-label">No Hp</label>
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Tanggal
+													Lahir</label>
 												<div class="col-sm-9">
-													<input type="email" class="form-control" id="inputEmail3"
+													<input type="date" class="form-control" id="inputEmail3"
+														value="<?= $pegawai['tgl_lahir'] ?>" name="tgl_lahir"
 														placeholder="No Hp">
+													<?= form_error('tgl_lahir', '<small class="text-danger pl-3">', '</small>'); ?>
+												</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Pndidikan
+													Terakhir</label>
+												<div class="col-sm-9">
+													<input type="text" class="form-control" id="inputEmail3"
+														value="<?= $pegawai['pnd_trkhr'] ?>" name="pnd_trkhr"
+														placeholder="Pendidikan Terakhir" readonly>
+													<?= form_error('pnd_trkhr', '<small class="text-danger pl-3">', '</small>'); ?>
+												</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Jurusan</label>
+												<div class="col-sm-9">
+													<input type="text" class="form-control" id="inputEmail3"
+														value="<?= $pegawai['jurusan'] ?>" name="jurusan"
+														placeholder="jurusan">
+													<?= form_error('jurusan', '<small class="text-danger pl-3">', '</small>'); ?>
 												</div>
 											</div>
 											<div class="form-group row">
 												<label for="inputEmail3" class="col-sm-3 col-form-label">Jabatan</label>
 												<div class="col-sm-9">
-													<input type="email" class="form-control" id="inputEmail3"
-														placeholder="Jabatan">
+													<input type="text" class="form-control" id="inputEmail3"
+														value="<?= $pegawai['jabatan'] ?>" name="jabatan"
+														placeholder="jabatan" readonly>
+													<?= form_error('jabatan', '<small class="text-danger pl-3">', '</small>'); ?>
 												</div>
 											</div>
-											<a href="<?= base_url('Console/pegawai') ?>" class="btn btn-light ">Ubah
-												Password</a>
-											<a href="<?= base_url('Console/pegawai') ?>" class="btn btn-light">Tutup</a>
-											<button type="submit" name="tambah"
-												class="btn btn-success float-right">Batal</button>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Nomor
+													HP</label>
+												<div class="col-sm-9">
+													<input type="text" class="form-control" id="inputEmail3"
+														value="<?= $pegawai['no_hp'] ?>" name="no_hp"
+														placeholder="no_hp">
+													<?= form_error('no_hp', '<small class="text-danger pl-3">', '</small>'); ?>
+												</div>
+											</div>
+
+											<div class="form-group row">
+												<div class="bg-info font-size-12 rounded-3 text-white p-3"><i
+														class="bi bi-info-circle-fill me-1"></i>Perubahan Password akan
+													berlaku
+													untuk seluruh akses pada platform lainnya</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Password
+													Lama</label>
+												<div class="col-sm-9">
+													<input type="password" class="form-control"
+														value="<?= set_value('pass_lama'); ?>" name="pass_lama"
+														placeholder="Isi Jika Ingin Mengganti Password"
+														id="password_old">
+													<?= form_error('pass_lama', '<small class="text-danger pl-3">', '</small>'); ?>
+												</div>
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Password
+													Baru</label>
+												<div class="col-sm-9">
+													<input type="password" class="form-control" id="input-password"
+														value="<?= set_value('pass_baru'); ?>" name="pass_baru"
+														placeholder="Masukkan password Baru">
+													<?= form_error('pass_baru', '<small class="text-danger pl-3">', '</small>'); ?>
+												</div>
+
+											</div>
+											<div class="form-group row">
+												<label for="inputEmail3" class="col-sm-3 col-form-label">Konfirmasi
+													Password
+													Baru</label>
+												<div class="col-sm-9">
+													<input type="password" class="form-control" id="input-password2"
+														value="<?= set_value('pass_baru2'); ?>" name="pass_baru2"
+														placeholder="Masukkan password Baru">
+												</div>
+
+											</div>
+											<a href="<?= base_url('Dashboard') ?>" class="btn btn-light">Tutup</a>
+											<button type="submit" name="tambah" class="btn btn-success float-right">Edit
+												Profile</button>
 										</form>
 									</div>
 								</div>
